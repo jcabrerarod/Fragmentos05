f066013bd195fc1dc1bbab5dcb1f522fc6945880
SARANAJA <jacinto@iesvirgendelcarmen.com>
2018-10-08 20:40:19 +0200

    Initial commit

---
+++ app/src/main/java/es/jcabrera/fragmentos05/Fragmento1.java
@@ -0,0 +1,34 @@
+package es.jcabrera.fragmentos05;
+
+import android.support.v4.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class Fragmento1 extends ListFragment {
+    private String[] artistas = { "Alberto Durero","Miguel Ángel","Rafael Sanzio","Tiziano ","Donatello ","Paolo Uccello","Giotto","Peter Breughel","F. Brunelleschi","Fra Angelico","A. Mantegna","Leonardo","G. Bellini","S. Botticelli","Cosimo Rosselli","Masaccio","Ghirlandaio","Giorgione","Filippino Lippi","P. della Francesca"};
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragmento1, container, false);
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setListAdapter(new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, artistas));
+    }
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        Toast.makeText(getActivity(), "Ha pulsado " + artistas[position],
+                Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/es/jcabrera/fragmentos05/Fragmentos05.java
@@ -0,0 +1,13 @@
+package es.jcabrera.fragmentos05;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Fragmentos05 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragmentos05);
+    }
+}
